cececa95d5bd8adcaf4279de4f37150628ac2259
Eden Mesfin <se.eden.mesfin@gmail.com>
2021-01-14 17:15:56 +0300

    week 2 updated

--- week_2/min stack.py
+++ week_2/min stack.py
@@ -1,26 +1,39 @@
-class MinStack:
+class MinStack(object):
 
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.stk=[]
+        self.min=[]
         
 
-    def push(self, x: int) -> None:
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
         self.stk.append(x)
-        
-            
-
-    def pop(self) -> None:
+        if len(self.min)>0:
+            self.min.append(min(x,self.min[-1]))
+        else:
+            self.min.append(x)
+    def pop(self):
+        """
+        :rtype: None
+        """
         self.stk.pop()
+        self.min.pop()
 
-    def top(self) -> int:
+    def top(self):
+        """
+        :rtype: int
+        """
         return self.stk[-1]
 
-    def getMin(self) -> int:
-        min=self.stk[0]
-        for i in self.stk:
-            if i<min:
-                min=i
-        return min
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        
+        return self.min[-1]
